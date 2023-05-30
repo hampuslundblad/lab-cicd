b5fdb00aef00d2db7e632b2e61c6c22217f49a63
Hampus Lundblad <hampus.lundblad@hotmail.se>
2023-05-30 16:49:48 +0200

    good dev

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> good dev</h1>
+        <h2> skriva rätt ip är svårt</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
